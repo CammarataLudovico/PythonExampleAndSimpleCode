9d5ec9965150b9cd35c8a82dec10eea1ae3e999d
Ludovico Cammarata <60526281+frosty04@users.noreply.github.com>
2020-03-12 23:34:06 +0100

    added some things and embellished the code

--- Script Calcolatrice per Legge di Ohm.py
+++ Script Calcolatrice per Legge di Ohm.py
@@ -1,27 +1,32 @@
-# Questo script calcola i valori di: Tensione;Corrente;Resistenza;Potenza, usati nella legge di Ohm con una tolleranza di 3 cifre dopo la virgola (errore di ± 0.001).
-
-print("1. Tensione" " (V)");
-print("2. Corrente" " (I)");
-print("3. Resistenza" " (R)");
-print("4. Potenza" " (P)");
-print("5. Esci dal programma");
+# Autore: Ludovico Cammarata
+# Versione 1.0.1
+# Questo script calcola i valori di: Tensione; Corrente; Resistenza; Potenza, usati nella legge di Ohm con una tolleranza di 3 cifre dopo la virgola (errore di ± 0.001).
+# Ho utilizzato '\n\r' così da andare a capo e rendere così l'output un pò più carino e meno incasinato
+print(" ================================================================")
+print("{Digita il numero corrispodente all'operazione che vuoi calcolare}")
+print(" ================================================================\n\r")
+print("1. Tensione." "          (Formula: R*I, espresso come V)");
+print("2. Corrente." "          (Formula: V/R, espresso come I)");
+print("3. Resistenza." "        (Formula: V/I, espresso come R)");
+print("4. Potenza." "           (Formula: V*I, espresso come P)");
+print("5. Esci dal programma.\n\r");
 scelta = int(input("Scegli cosa calcolare: "));
 if (scelta>=1 and scelta<=4):
-    print("Scrivi 2 valori: ");
+    print("Scrivi i 2 valori (uno alla volta): ");
     valore1 = float(input());
     valore2 = float(input());
     if scelta == 1:
     	res = valore1*valore2;
-    	print("Il tuo risultato è = ", "%.3f" " Volt" %res);
+    	print("\n\rIl tuo risultato è = ", "%.3f" " Volt" %res);
     elif scelta == 2:
     	res = valore1/valore2;
-    	print("Il tuo risultato è = ", "%.3f" " Ampere" %res);
+    	print("\n\rIl tuo risultato è = ", "%.3f" " Ampere" %res);
     elif scelta == 3:
     	res = valore1/valore2;
-    	print("Il tuo risultato è = ", "%.3f" " Ohm" %res);
+    	print("\n\rIl tuo risultato è = ", "%.3f" " Ohm" %res);
     elif scelta == 4:
     	res = valore1*valore2;
-    	print("Il tuo risultato è = ", "%.3f" " Watt" %res);
+    	print("\n\rIl tuo risultato è = ", "%.3f" " Watt" %res);
 elif scelta == 5:
     exit();
 else:
